f927c87695b618614d99894e5e2ce53b23a06c36
Léo Geison <leogeisom@gmail.com>
2023-04-27 23:35:58 -0300

    front-end rocketnotes

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Header } from '../../componentes/Header';
+import { Input } from '../../componentes/Input';
+import { Textarea } from '../../componentes/Textarea';
+import { Section } from '../../componentes/Section';
+import { NoteItem } from '../../componentes/NoteItem';
+import { Button } from '../../componentes/Button';
+import { ButtonText } from '../../componentes/ButtonText';
+
+import { Container, Form } from './styles';
+import { api } from '../../services/api';
+
+export function New() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState('');
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState('');
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink('');
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag('');
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  }
+
+  async function handleNewNote() {
+    if (!title) {
+      return alert('Digite o titulo da nota');
+    }
+
+    if (newLink) {
+      return alert(
+        'Voce deixou um link no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.'
+      );
+    }
+
+    if (newTag) {
+      return alert(
+        'Voce deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.'
+      );
+    }
+
+    await api.post('/notes', { title, description, tags, links });
+
+    alert('Nota criada com sucesso!');
+    navigate(-1);
+  }
+
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <ButtonText title="voltar" onClick={handleBack} />
+          </header>
+
+          <Input
+            placeholder="Título"
+            onChange={e => setTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder="Observações"
+            onChange={e => setDescription(e.target.value)}
+          />
+
+          <Section title="Links úteis">
+            {links.map((link, index) => (
+              <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => handleRemoveLink(link)}
+              />
+            ))}
+
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              {tags.map((tag, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
+              ))}
+
+              <NoteItem
+                isNew
+                placeholder="Nova Tag"
+                onChange={e => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
+            </div>
+          </Section>
+
+          <Button title="Salvar" onClick={handleNewNote} />
+        </Form>
+      </main>
+    </Container>
+  );
+}
